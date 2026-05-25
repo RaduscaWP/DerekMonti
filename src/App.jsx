f7aa95b132deaeed16b1d8d527ee356f167c63a1
Radusca <grozavradustefan@gmail.com>
2026-05-25 18:04:39 +0300

    feat: add @vercel/analytics for enhanced analytics tracking in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Layout from './components/layout/Layout.jsx';
 import Home from './pages/Home.jsx';
@@ -19,15 +20,18 @@ function ScrollToTop() {
 
 export default function App() {
   return (
-    <Layout>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogArticle />} />
-      </Routes>
-    </Layout>
+    <>
+      <Layout>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogArticle />} />
+        </Routes>
+      </Layout>
+      <Analytics />
+    </>
   );
 }
